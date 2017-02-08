5fa34bdf778a5ef8a3a1f036fcf923201c241725
ComSecNinja <timo@comsec.ninja>
2017-02-08 02:50:12 +0200

    Fix uuid algo

--- server.js
+++ server.js
@@ -186,5 +186,5 @@ server = ws.createServer(function(conn) {
 }).listen(process.env.PORT || 8000);
 
 function uuid(a){
-    return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)
+    return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuid)
 }
